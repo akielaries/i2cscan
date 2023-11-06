5f8f8f151a45c2919ecdcf33edf48bd503ff37da
akielaries <akiel@akiel.org>
2023-11-06 03:58:34 +0000

    updates to command line args and options

--- i2cscan.c
+++ i2cscan.c
@@ -16,60 +16,75 @@
 
 // address info struct?
 
+// scan for available I2C busses
 static void bus_scan() {
     printf("SCAN FOR I2C BUSSES\n");
 }
 
-//static void addr_scan(const char *bus,
-//              unsigned char found_addrs[],
-//              size_t *num_found_addrs) {
+// find available addresses on a given I2C bus
 static void addr_scan(const char *bus, int min, int max) {
     int file;
 
-    // Iterate through the specified I2C address range
+    // iterate through the specified I2C address range
     for (int address = min; address <= max; address++) {
-        // Attempt to open the I2C bus
+        // attempt to open the I2C bus
         file = open(bus, O_RDWR);
         if (file < 0) {
-            perror("[!] Failed to open I2C bus");
+            perror("Failed to open I2C bus");
             return;
         }
 
-        // Set the slave address for the current iteration
+        // set the slave address for the current iteration
         if (ioctl(file, I2C_SLAVE, address) < 0) {
             close(file);
-            continue; // No device at this address
+            continue; // no device at this address
         }
 
-        // Attempt to read one byte from the address to check if it responds
+        // attempt to read one byte from the address to check if it responds
         unsigned char buffer;
         if (read(file, &buffer, 1) == 1) {
-            // Device found and responded, print the address
+            // device found and responded, print the address
             printf("Found device at I2C address 0x%02X\n", address);
         }
 
-        // Close the file
+        // close the file
         close(file);
     }
 }
 
+// fetch all I2C busses and their addresses
 static void fetch_all() {
     printf("get every I2C bus and every address on them\n");
 }
 
 // i2cscan binary CLI usage
 static void usage(const char *bin) {
-    printf("Usage: %s [-v] [-b] BUS [-a | -r (FIRST LAST)]\n", bin);
+    printf("Usage: %s [-h] [-v] [-l] [-f] [-b] BUS [-a | -r FIRST-LAST]\n", bin);
     printf("BUS is an integer representing the I2C bus name\n");
-    printf("FIRST LAST represents the MIN_ADDR/MAX_ADDR addresses to scan\n");
+    printf("FIRST LAST represents the MIN_ADDR/MAX_ADDR addresses to scan\n\n");
 }
 
+static void help() {
+    printf("\nAvailable options:\n"
+            "   -h help\n"
+            "   -v version\n"
+            "   -l lists all available I2C busses\n"
+            "   -f fetch all busses & available addresses\n"
+            "   -b specify I2C bus\n"
+            "   -a scan all addresses\n"
+            "   -r scan addresses between FIRST & LAST\n"
+            "\n");
+}
+
+// main driver featuring cmd line parsing
 int main (int argc, char *argv[]) {
+    int flag_bus = 0, 
+        flag_all_addrs = 0, 
+        flag_range_addrs = 0, 
+        flag_list_busses =0,
+        flag_fetch_all = 0;
     int option;
-    int bus = -1;
-    //const char *bus_file;
     char bus_file[20];
-    int flag_all_addrs, flag_range_addrs;
     int MIN_ADDR = 0, MAX_ADDR = 127;
 
     if (argc < 2) {
@@ -77,56 +92,90 @@ int main (int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    while ((option = getopt(argc, argv, "vb:ar:")) != -1) {
+    while ((option = getopt(argc, argv, "hvb:ar:")) != -1) {
         switch(option) {
+            // help
+            case 'h':
+                help();
+                exit(EXIT_FAILURE);
+                break;
+
             // version
             case 'v':
                 printf("v%s\n", VERSION);
+                exit(EXIT_FAILURE);
                 break;
 
+            case 'l':
+                printf("list something?\n");
+                flag_list_busses = 1;
+                break;
+            
+            case 'f':
+                printf("get everything!\n");
+                flag_fetch_all = 1;
+                break;
+            
             // bus
             case 'b':
-                bus = atoi(optarg);
                 snprintf(bus_file, sizeof(bus_file), "/dev/i2c-%s", optarg);
-
-                printf("Bus = %s\n", bus_file);
+                // set flag for bus
+                flag_bus = 1;
                 break;
 
             // all addresses
             case 'a':
-                printf("Scanning all addresses %d to %d\n", MIN_ADDR, MAX_ADDR);
+                if (!flag_bus) {
+                    printf("an I2C bus must be specified with -b\n");
+                    usage(argv[0]);
+                    exit(EXIT_FAILURE);
+                }
                 // set flag for all addrs
                 flag_all_addrs = 1;
                 break;
 
             // range of addresses
             case 'r':
+                if (!flag_bus) {
+                    printf("an I2C bus must be specified with -b\n");
+                    usage(argv[0]);
+                    exit(EXIT_FAILURE);
+                }
                 if (sscanf(optarg, "%d-%d", &MIN_ADDR, &MAX_ADDR) != 2) {
                     fprintf(stderr, "Invalid range format. Use -r MIN_ADDR-MAX_ADDR\n");
-                    exit(EXIT_FAILURE);;
+                    exit(EXIT_FAILURE);
                 }
-                printf("Scanning addresses from %d to %d\n", MIN_ADDR, MAX_ADDR);
                 // set flag for range of addrs
                 flag_range_addrs = 1;
                 break;
 
             case '?':
                 usage(argv[0]);
+                exit(EXIT_FAILURE);
                 break;
             
             default:
                 usage(argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
 
     // some error checking 
-    if (bus == -1) {
-        // FIXME
-        fprintf(stderr, "FIXME Bus is required. Usage: %s -b BUS\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if (flag_bus) {
+        if (flag_all_addrs || flag_range_addrs) {
+            printf("Scanning all addresses %d to %d\n", MIN_ADDR, MAX_ADDR);
+            addr_scan(bus_file, MIN_ADDR, MAX_ADDR);
+        }
+        else {
+            printf("-b requires -a for all addresses or -r for a range\n");
+            usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
     }
-    if (flag_all_addrs || flag_range_addrs) {
-        addr_scan(bus_file, MIN_ADDR, MAX_ADDR);
+    else {
+        // FIXME
+        usage(argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     return 0;
